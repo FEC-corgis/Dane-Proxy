67f6f3007b68f6ad5e2bfc47f3154a5dbaaef1d5
Dane Murphy <danem7@gmail.com>
2021-01-22 22:24:54 -0800

    reverse proxy setup with production webpack

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { join } = require('path');
+const headerServiceRoutes = require('./routes/headerRoutes');
+const app = express();
+const port = process.env.PORT || 80;
+
+const publicPath = join(__dirname, 'dist');
+
+app.use(express.static(publicPath));
+app.use('/api/headerService', headerServiceRoutes);
+
+app.get('*', (_req, res) => {
+    res.sendFile(join(publicPath, 'index.html'), (err) => {
+        if (err) throw err;
+    });
+});
+
+app.listen(port, () => console.log(`Proxy started on port ${port}...`));
---
+++ webpack.prod.js
@@ -0,0 +1,18 @@
+const common = require('./webpack.common');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { merge } = require('webpack-merge');
+const { resolve } = require('path');
+
+module.exports = merge(common, {
+    mode: 'production',
+    output: {
+        filename: 'main.js',
+        path: resolve(__dirname, 'dist'),
+        publicPath: '/',
+    },
+    plugins: [new CleanWebpackPlugin()],
+    performance: {
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000,
+    },
+});
