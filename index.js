9e01ec20d370b08a1605ace773aabdd30d791a8b
Dane Murphy <danem7@gmail.com>
2021-01-30 13:26:24 -0800

    static files are now served from s3 bucket

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const express = require('express');
-const { join } = require('path');
+const { S3 } = require('aws-sdk');
 const headerArgs = require('./routes/headerRoutes');
 const hostedbyArgs = require('./routes/hostedbyRoutes');
 
+const s3 = new S3();
 const app = express();
 const port = process.env.PORT || 5000;
-const publicPath = join(__dirname, 'dist');
-
-app.use(express.static(publicPath, { maxAge: 3600000 }));
 
 app.use(...headerArgs);
 app.use(...hostedbyArgs);
 
 app.get('*', (_req, res) => {
-    res.sendFile(join(publicPath, 'index.html'), (err) => {
-        if (err) throw err;
-    });
+    const params = {
+        Bucket: 'fec-corgis',
+        Key: 'static/index.html',
+    };
+
+    s3.getObject(params)
+        .on('httpHeaders', (code, headers) => {
+            res.set('Cache-Control', 'public, max-age=31557600');
+        })
+        .createReadStream()
+        .pipe(res);
 });
 
 app.listen(port, () => console.log(`Proxy started on port ${port}...`));
