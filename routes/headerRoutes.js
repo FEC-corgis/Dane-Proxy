af638441ab388107f9250b423528a5184197cc34
Dane Murphy <danem7@gmail.com>
2021-01-31 21:22:46 -0800

    cache handled

--- routes/headerRoutes.js
+++ routes/headerRoutes.js
@@ -1,8 +1,18 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const { headerCache } = require('../middleware/headerCache');
+const client = require('../constants/client');
 
 module.exports = [
     '/api/headerService/:propertyId',
     headerCache,
-    createProxyMiddleware('http://localhost:5001'),
+    createProxyMiddleware({
+        target: 'http://localhost:5001',
+        onProxyRes: (proxyRes, req, _res) => {
+            const { propertyId } = req.params;
+            proxyRes.on('data', (data) => {
+                const cacheData = data.toString();
+                client.setex(`header${propertyId}`, 3600, cacheData);
+            });
+        },
+    }),
 ];
--- routes/hostedbyRoutes.js
+++ routes/hostedbyRoutes.js
@@ -1,8 +1,18 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const { hostedbyCache } = require('../middleware/hostedbyCache');
+const client = require('../constants/client');
 
 module.exports = [
     '/api/hostedbyService/:propertyId',
     hostedbyCache,
-    createProxyMiddleware('http://localhost:5002'),
+    createProxyMiddleware({
+        target: 'http://localhost:5002',
+        onProxyRes: (proxyRes, req, _res) => {
+            const { propertyId } = req.params;
+            proxyRes.on('data', (data) => {
+                const cacheData = data.toString();
+                client.setex(`hostedby${propertyId}`, 3600, cacheData);
+            });
+        },
+    }),
 ];
