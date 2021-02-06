3efa95ea0b42c075d868f1e572f6496d14120d62
Dane Murphy <danem7@gmail.com>
2021-02-06 12:20:48 -0800

    finializing production changes

--- index.js
+++ index.js
@@ -4,13 +4,10 @@ const headerArgs = require('./routes/headerRoutes');
 const hostedbyArgs = require('./routes/hostedbyRoutes');
 const reviewsArgs = require('./routes/reviewsRoutes');
 const entirehouseArgs = require('./routes/entirehouseRoutes');
-const { join } = require('path');
 
 const s3 = new S3();
 const app = express();
 const port = process.env.PORT || 5000;
-
-app.use(express.static(join(__dirname, 'public')));
 
 app.use(...headerArgs);
 app.use(...hostedbyArgs);
