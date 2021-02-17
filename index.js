6e89f45860e0b5ce88f5b6383fbc492393467eff
Dane Murphy <danem7@gmail.com>
2021-02-17 13:22:52 -0800

    entirehouse routes added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { S3 } = require('aws-sdk');
 const headerArgs = require('./routes/headerRoutes');
 const hostedbyArgs = require('./routes/hostedbyRoutes');
 const reviewsArgs = require('./routes/reviewsRoutes');
+const entireHouseArgs = require('./routes/entirehouseRoutes');
 
 const s3 = new S3();
 const app = express();
@@ -11,6 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(...headerArgs);
 app.use(...hostedbyArgs);
 app.use(...reviewsArgs);
+app.use(...entireHouseArgs);
 
 app.get('*', (_req, res) => {
     const params = {
